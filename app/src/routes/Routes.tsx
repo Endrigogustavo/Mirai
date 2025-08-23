08b6ed0f5864a5bf464d27240de78633fa5cdb59
nickolss <nickolasmaiaraujo@gmail.com>
2025-08-23 17:26:43 -0300

    fix: removed foot in login and signup screen

--- app/src/routes/Routes.tsx
+++ app/src/routes/Routes.tsx
@@ -1,4 +1,9 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Footer } from "../components/Footer";
 import { Home, History, Forms, Result, Settings } from "../screens";
@@ -15,24 +20,58 @@ export type RootStackParamList = {
   Result: { answers: (number | null)[] };
 };
 
+const screensWithoutFooter: Array<keyof RootStackParamList> = [
+  "Login",
+  "SignUp",
+];
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export const Routes = () => {
+const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="History" component={History} />
-        <Stack.Screen name="Forms" component={Forms} />
-        <Stack.Screen name="Result" component={Result} />
-        <Stack.Screen name="Settings" component={Settings} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-      <Footer />
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="History" component={History} />
+      <Stack.Screen name="Forms" component={Forms} />
+      <Stack.Screen name="Result" component={Result} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
   );
 };
+
+export const Routes = () => {
+  const [routeName, setRouteName] = useState<
+    keyof RootStackParamList | undefined
+  >("Login");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <NavigationContainer
+          onStateChange={(state) => {
+            if (state) {
+              const currentRoute = state.routes[state.index];
+              const currentRouteName =
+                getFocusedRouteNameFromRoute(currentRoute) ?? currentRoute.name;
+              setRouteName(currentRouteName as keyof RootStackParamList);
+            }
+          }}
+        >
+          <AppNavigator />
+          {routeName && !screensWithoutFooter.includes(routeName) && <Footer />}
+        </NavigationContainer>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
